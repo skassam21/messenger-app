dfbba34bcd7ebe9e31a0aca2cbd9ba17999610af
Shums Kassam <skassam21@gmail.com>
2021-01-14 20:03:52 -0500

    Add login / sign up V1

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
-
-import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import { theme } from "./themes/theme.js";
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 import "./App.css";
 
 function App() {
+  const [loggedIn, setLoggedIn] = React.useState(true)
+
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Route exact path="/">
+          {loggedIn ? <Redirect to="/signup" /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+
       </BrowserRouter>
     </MuiThemeProvider>
   );
